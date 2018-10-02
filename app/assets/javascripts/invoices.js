bdccd70d6c0f0f15b8e368af539d60e8e9d6eba9
zn <244305150@qq.com>
2018-10-02 23:27:42 +0800

    添加发票数据统计功能

--- app/assets/javascripts/invoices.js
+++ app/assets/javascripts/invoices.js
@@ -1,28 +1,77 @@
-$().ready(function() {
+$(".invoices.edit, .invoices.edit").ready(function() {
   $(".js-amount, .js-tax-unit-price").keyup(function() {
-    var amount = $(".js-amount").val();
-    var cess = $(".js-cess").val(); 
-    var price = $(".js-tax-unit-price").val();
-    var total = parseFloat(amount)*parseFloat(price);
-    $(".js-tax-total").val(total);
-
-    money_calculator(parseFloat(price), parseFloat(amount), parseFloat(cess))
+    write_money_result();
   });
 
   $(".js-cess").change(function() {
-    var that = this;
-    var cess = $(that).val(); 
-    var amount = $(".js-amount").val();
-    var price = $(".js-tax-unit-price").val();
-    money_calculator(parseFloat(price), parseFloat(amount), parseFloat(cess))
+    write_money_result();
   });
 })
 
+$(".invoices.index").ready(function() {
+  tax_count();
+  $(".js-index-cess").change(function() {
+    var that = this;
+    var cess_val = $(that).val(); 
+    $(".invoice-body-tr").each(function() {
+      var cess = $(this).find(".invoice-index-cess");
+      var amount = $(this).find(".invoice-index-amount");
+      var price = $(this).find(".invoice-index-price");
+
+      var result = money_calculator(parseFloat(price.text()), parseFloat(amount.text()), parseFloat(cess_val))
+
+      var unprice = $(this).find(".invoice-index-unprice");
+      var untotal = $(this).find(".invoice-index-untotal");
+      var money = $(this).find(".invoice-index-money");
+
+      cess.text(cess_val*100 + "%");
+      unprice.text(result.price.toString());
+      untotal.text(result.total);
+      money.text(result.money);
+    });
+    tax_count();
+  });
+});
+
+function tax_count() {
+  var untax_count = 0;
+  var intax_count = 0;
+  var tax_count = 0;
+  $(".invoice-body-tr").each(function() {
+    var total = $(this).find(".invoice-index-total");
+    var untotal = $(this).find(".invoice-index-untotal");
+    var money = $(this).find(".invoice-index-money");
+
+    untax_count += parseFloat(total.text());
+    intax_count += parseFloat(untotal.text());
+    tax_count += parseFloat(money.text());
+  });
+  $(".untax-count").html(untax_count.toFixed(2) + "&nbsp;&nbsp;&nbsp;");
+  $(".intax-count").html(intax_count.toFixed(2) + "&nbsp;&nbsp;&nbsp;");
+  $(".tax-count").html(tax_count.toFixed(2));
+}
+function write_money_result() {
+  var cess = $(".js-cess").val(); 
+  var amount = $(".js-amount").val();
+  var price = $(".js-tax-unit-price").val();
+
+  var total = parseFloat(amount)*parseFloat(price);
+  var result = money_calculator(parseFloat(price), parseFloat(amount), parseFloat(cess))
+
+  $(".js-tax-total").val(total);
+  $(".js-untax-unit-price").val(result.price);
+  $(".js-untax-total").val(result.total);
+  $(".js-tax-money").val(result.money);
+}
+
 function money_calculator(tax_unit_price, amount, cess) {
-  var untax_unit_price = tax_unit_price/(1+cess);
+  var untax_unit_price = (tax_unit_price/(1+cess)).toFixed(10);
   var untax_total = ((amount*tax_unit_price)/(1+cess)).toFixed(2);
   var tax_money = (untax_total*cess).toFixed(2);
-  $(".js-untax-unit-price").val(untax_unit_price);
-  $(".js-untax-total").val(untax_total);
-  $(".js-tax-money").val(tax_money);
+  var result = {
+    price: untax_unit_price,
+    total: untax_total,
+    money: tax_money
+  }
+  return result;
 }
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,18 +2,20 @@
 #
 # Table name: invoices
 #
-#  id              :integer          not null, primary key
-#  standard        :string           default(""), not null
-#  unit            :string           default(""), not null
-#  amount          :float            default(0.0), not null
-#  tax_unit_price  :float            default(0.0), not null
-#  tax_total       :float            default(0.0), not null
-#  cess            :float            default(0.0), not null
-#  tax_money       :float            default(0.0), not null
-#  tax_category_id :integer
-#  buyer_id        :integer
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
+#  id               :integer          not null, primary key
+#  standard         :string           default(""), not null
+#  unit             :string           default(""), not null
+#  amount           :float            default(0.0), not null
+#  tax_unit_price   :float            default(0.0), not null
+#  tax_total        :float            default(0.0), not null
+#  cess             :float            default(0.0), not null
+#  untax_unit_price :float            default(0.0), not null
+#  untax_total      :float            default(0.0), not null
+#  tax_money        :float            default(0.0), not null
+#  tax_category_id  :integer
+#  buyer_id         :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
 #
 
 class Invoice < ActiveRecord::Base
